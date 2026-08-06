d4ffa5729f620295c8c513a22f6107ac756701de
E-d-o <99652381+E-d-o@users.noreply.github.com>
2026-08-06 19:31:29 +0200

    basic ui and adding book operation

--- script.js
+++ script.js
@@ -66,5 +66,27 @@ function printLibrary() {
 createNewBook(new BookQualities("Gennaro stirato"))
 createNewBook(new BookQualities("La principessa", "Toyo Taro"))
 console.log(library)
-
 printLibrary();
+
+new_book_button = document.querySelector("#new_book")
+function new_book_onclick(event) {
+    console.log("I can now add a book!")
+    field = document.querySelector("fieldset")
+    field.style.display = "block";
+
+}
+new_book_button.addEventListener("click", new_book_onclick)
+function on_submit(event) {
+    event.preventDefault();
+    console.log("Sumbitted!");
+
+    field = document.querySelector("fieldset")
+    field.style.display = "none"
+    form = document.querySelector("form")
+    formData = new FormData(form)
+    createNewBook(new BookQualities(formData.get("name"), formData.get("author"), formData.get("year")))
+    library[library.length - 1].toString();
+}
+
+form = document.querySelector("form")
+form.addEventListener("submit", on_submit)
